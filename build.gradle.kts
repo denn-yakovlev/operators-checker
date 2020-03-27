efaa8eda98c8d1b073287f6967e0bc9d0c958078
Denis Yakovlev <denn.yakovlev@gmail.com>
2020-03-27 20:09:30 +0300

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    kotlin("jvm") version "1.3.61"
+    kotlin("plugin.serialization") version "1.3.61"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    jcenter()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-runtime:0.20.0")
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+}
---
+++ src/main/kotlin/OperatorsChecker.kt
@@ -0,0 +1,9 @@
+import kotlinx.serialization.*
+import kotlinx.serialization.json.*
+
+class OperatorsChecker {
+    val json = Json(JsonConfiguration.Stable)
+    fun check()  {
+
+    }
+}
